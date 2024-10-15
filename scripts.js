7865ed338506955692226c7d2aa909f6cf2520a0
leejaewon <leejaewon6463@icloud.com>
2024-10-16 02:44:02 +0900

    ~~~

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@
 document.getElementById('myButton').addEventListener('click', function() {
     window.location.href = 'https://github.com/coogie22'; // 원하는 링크로 변경
 });
+
+setInterval(function() {
+    document.querySelector('.container input').click();
+  }, 300); // 1000ms = 1초
